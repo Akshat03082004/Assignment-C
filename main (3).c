1f3944c9255152c3024e421f4010b8d0b032d4ce
Akshat03082004 <Akshatsharma152739@gmail.com>
2025-03-21 13:31:32 +0530

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    printf("\n5 countries:\nIndia\nRussia\nUSA\nFrance\nChina\n");
+
+    printf("\n5 states:\nGujrat\nGoa\nKerela\nBihar\nLadakh\n");
+    
+    printf("\n5 cities:\nKota\nJaipur\nBundi\nTonk\nRawatbhata\n");
+    
+    printf("\n5 rivers:\nJhelum\nChenab\nRavi\nBeas\nSutlej\n");
+    
+    printf("\n5 friends:\nAnirudh\nRahul\nAshu\nAlfez\nIshan");
+    
+}
