07b52833a3951bb2008924c331748678498ba68d
Akshat03082004 <Akshatsharma152739@gmail.com>
2025-04-05 10:53:30 +0530

    Add files via upload

---
+++ vowel consonent.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+   char ch;
+   printf("Enter the character ");
+   scanf("%c",&ch);
+   if(ch=='a')
+   {printf("Vowel");}
+
+     else if(ch=='e')
+   {printf("Vowel");}
+
+    else  if(ch=='i')
+   {printf("Vowel");}
+
+    else if(ch=='o')
+   {printf("Vowel");}
+
+    else if(ch=='u')
+   {printf("Vowel");}
+
+    else{
+    printf("Consonent");
+}
+}
+    
